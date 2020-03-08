d220a0c4b15a5a79f1243481b946dd7f040e5482
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-08 15:05:05 +0530

    ADDED : win or loose

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,5 +2,17 @@
 echo "WelCome to Gambling Simulation"
 
 #CONSTANT VARIABLES
-STAKE=100
+MYSTAKE=100
 BET=1
+
+#CHECK WIN OR LOOSE
+function checkWinORLoose()
+	{
+		if [[ $((RANDOM%2)) == 1 ]]
+		then
+			echo "win"
+		else
+			echo "loose"
+		fi
+	}
+checkWinORLoose
