c1384b73b6a17026996c526860da76e8d86a122f
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-08 15:26:50 +0530

    ADDED : check win or loose 50% of stake

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -4,15 +4,23 @@ echo "WelCome to Gambling Simulation"
 #CONSTANT VARIABLES
 MYSTAKE=100
 BET=1
-
+cash=$MYSTAKE
+FIFTYPERCENTSTAKE=$(($MYSTAKE*50/100))
+MIN_STAKE=$(($MYSTAKE - $FIFTYPERCENTSTAKE))
+MAX_STAKE=$(($MYSTAKE + $FIFTYPERCENTSTAKE))
 #CHECK WIN OR LOOSE
 function checkWinORLoose()
 	{
-		if [[ $((RANDOM%2)) == 1 ]]
-		then
-			echo "win"
-		else
-			echo "loose"
-		fi
+		while [[ $cash -lt $MAX_STAKE && $cash -gt $MIN_STAKE ]]
+		do
+
+			if [[ $((RANDOM%2)) == 1 ]]
+			then
+				cash=$((cash+BET))
+			else
+				cash=$((cash-BET))
+			fi
+		done
+		echo "$cash"
 	}
 checkWinORLoose
