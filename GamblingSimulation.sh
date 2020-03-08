d5fd8541a442647823f9a8b4337233fd670ec101
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-08 16:07:58 +0530

    ADDED : total amount of 20 days

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -4,6 +4,9 @@ echo "WelCome to Gambling Simulation"
 #CONSTANT VARIABLES
 MYSTAKE=100
 BET=1
+TOTAL_AMOUNT=0
+TOTAL_DAYS=20
+
 cash=$MYSTAKE
 FIFTYPERCENTSTAKE=$(($MYSTAKE*50/100))
 MIN_STAKE=$(($MYSTAKE - $FIFTYPERCENTSTAKE))
@@ -11,16 +14,27 @@ MAX_STAKE=$(($MYSTAKE + $FIFTYPERCENTSTAKE))
 #CHECK WIN OR LOOSE
 function checkWinORLoose()
 	{
-		while [[ $cash -lt $MAX_STAKE && $cash -gt $MIN_STAKE ]]
+		for ((day=1;day<$TOTAL_DAYS;day++))
 		do
 
-			if [[ $((RANDOM%2)) == 1 ]]
-			then
-				cash=$((cash+BET))
-			else
-				cash=$((cash-BET))
-			fi
+			while [[ $cash -lt $MAX_STAKE && $cash -gt $MIN_STAKE ]]
+			do
+
+				if [[ $((RANDOM%2)) == 1 ]]
+				then
+					cash=$((cash+BET))
+				else
+					cash=$((cash-BET))
+				fi
+			done
+			TOTAL_AMOUNT=$(($TOTAL_AMOUNT+$cash-$MYSTAKE))
 		done
-		echo "$cash"
+		
+		if [[ $TOTAL_AMOUNT -gt 0 ]]
+		then
+			echo "Total Amount win :" $TOTAL_AMOUNT
+		else
+			echo "Total Amount Loose :" $TOTAL_AMOUNT
+		fi
 	}
 checkWinORLoose
